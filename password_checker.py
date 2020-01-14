8fa0a372bdd958566f9a99ec5f609b635f3d5053
Thabo Mayika <mayikathabo@gmail.com>
2020-01-14 13:44:44 +0200

    second commit

--- password_checker.py
+++ password_checker.py
@@ -1,53 +1,53 @@
 import re
-# def password_is_valid(password):
-#     ## password should exist
-#     if (password != None):
-#         print("password exist")
-#     else:
-#         raise Exception("password should exist")
+def password_is_valid(password):
+     ## password should exist
+    if (password != None):
+        print("password exist")
+    else:
+        raise Exception("password should exist")
     
     
-#     ## length of password is long enough
+     ## length of password is long enough
     
-#     if len(password) > 8:
-#        print("Valid length")
-#     else:
-#         raise Exception('password should be longer than than 8 characters')
+    if len(password) > 8:
+        print("Valid length")
+    else:
+        raise Exception('password should be longer than than 8 characters')
 
-#     ## lowercase characters
+     ## lowercase characters
     
-#     character = r'[a-z]'
+    character = r'[a-z]'
 
-#     for c in character:
-#         if c != character: 
-#             print("Valid atleast one lowercase letter")
-#         else:
-#             raise Exception("password should have at least one lowercase letter")
+    for c in character:
+        if c != character: 
+            print("Valid atleast one lowercase letter")
+        else:
+            raise Exception("password should have at least one lowercase letter")
 
-#     ## Uppercase characters 
+     ## Uppercase characters 
      
-#     characters = r'[A-Z]'             
-#     for i in characters: 
-#         if i != characters:
-#             print("Valid atleast one uppercase letter")
-#         else:
-#             raise Exception("password should have at least one uppercase letter") 
+    characters = r'[A-Z]'             
+    for i in characters: 
+        if i != characters:
+            print("Valid atleast one uppercase letter")
+        else:
+            raise Exception("password should have at least one uppercase letter") 
 
-#     # Digits 
+     # Digits 
 
-#     if re.search('[0-9]',password):
-#        print("valid number of Digits")
-#     else:
-#         raise Exception('password should at least have one digit')
+    if re.search('[0-9]',password):
+        print("valid number of Digits")
+    else:
+        raise Exception('password should at least have one digit')
 
-#     #special characters
+     #special characters
     
-#     # special = r"[+]"
-#     # for s in special:
-#     if re.search(r"[+]",password):
-#         print("valid number of special characters")
-#     else:
-#         raise Exception('password should have at least one special character')
+     # special = r"[+]"
+     # for s in special:
+    if re.search(r"[+]",password):
+        print("valid number of special characters")
+    else:
+        raise Exception('password should have at least one special character')
 
 
 
--- test_password_is_valid.py
+++ test_password_is_valid.py
@@ -7,10 +7,10 @@ def test_password_is_valid():
 
         with pytest.raises(Exception):
                 assert password_is_valid('') == "password should exist"
-                # assert password_is_valid('TTTTTTTTT') == 'password should have atleast one lowercase letter'
-                # assert password_is_valid('ttttttttt') == 'password should atleast have one uppercase letter'
-                # assert password_is_valid('tTttttttt') == 'password should atleast have one digit'
-                # assert password_is_valid('tVp9rart') == 'password should be longer than than 8 characters'
+                assert password_is_valid('TTTTTTTTT') == 'password should have atleast one lowercase letter'
+                assert password_is_valid('ttttttttt') == 'password should atleast have one uppercase letter'
+                assert password_is_valid('tTttttttt') == 'password should atleast have one digit'
+                assert password_is_valid('tVp9rart') == 'password should be longer than than 8 characters'
         
         
         
